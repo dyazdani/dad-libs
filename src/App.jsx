b5f631c3397b4794e5a16f2d10aefe19205c60af
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-04-11 05:43:55 -0700

    Adding imports to App.jsx and exports to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
+import Header from './components/common/Header';
+import Button from './components/common/Button';
+import StartPage from './components/start/StartPage';
+import StoryPage from './components/story/StoryPage';
+import WordEntryPage from './components/word_entry/WordEntryPage';
 
 const App = () => {
     return <div>Hello World</div>
