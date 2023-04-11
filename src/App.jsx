ea770c28173486fffca689183f57dbb2553d2e1e
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-04-11 13:01:15 -0700

    Added word entry page functionality

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,36 @@ import WordEntryPage from './components/word_entry/WordEntryPage';
 const wordTypeList = ['Plural Noun', 'Adjective', 'Noun', 'Adjective', 'Plural Body Part', 'Plural Noun', 'Verb Ending in -ing', 'Plural Noun', 'Verb', 'Noun', 'Verb', 'Noun', 'Verb Ending in -ing', 'Plural Noun', 'Celebrity']
 
 const App = () => {
-    const [activePage, setActivePage] = useState('start');
+    const [activePage, setActivePage] = useState('word_entry');
     const [chosenWords, setChosenWords] = useState([]);
 
     function handleStartPageButtonClick() {
         setActivePage('word_entry');
     }
 
-    function handleAddWordButtonClick(word) {
+    // helper function
+    function addWord() {
+        const input = document.querySelector('input');
+        const word = input.value;
         const newChosenWords = [...chosenWords, word];
         setChosenWords(newChosenWords);
+        input.value = '';
+    }
+    function handleAddWordButtonClick(e) {
+        e.preventDefault();        
+        if (chosenWords.length < wordTypeList.length - 1) {
+            addWord();
+        } else {
+            addWord();
+            
+        }
+
+        
     }    
+
+    function handleGenerateStoryButtonClick() {
+        setActivePage('story');
+    }
 
     let pageContent;
 
@@ -28,7 +47,7 @@ const App = () => {
     }
 
     if (activePage === 'word_entry') {
-        pageContent = <WordEntryPage wordTypeList={wordTypeList} handleClick={handleAddWordButtonClick}></WordEntryPage> //TODO: made handleClick an anonymous func to pass argument
+        pageContent = <WordEntryPage wordTypeList={wordTypeList} chosenWords={chosenWords} handleAddWordClick={handleAddWordButtonClick} handleGenerateStoryClick={handleGenerateStoryButtonClick}></WordEntryPage>
 
     }
 
--- src/components/word_entry/WordEntryPage.jsx
+++ src/components/word_entry/WordEntryPage.jsx
@@ -1,11 +1,21 @@
 import Button from "../common/Button";
 
-export default function WordEntryPage({wordTypeList, handleClick}) {
-    return (
-            <form>
-                <label></label>
-                <input></input>
-                <Button onClick={handleClick} buttonText='Add Word'></Button>
-            </form>
-    )
+export default function WordEntryPage({chosenWords, wordTypeList, handleAddWordClick, handleGenerateStoryClick}) {
+    let currentWordType = wordTypeList[chosenWords.length];
+
+    if (chosenWords.length < wordTypeList.length) {
+        return (
+            <div>
+                <label htmlFor="word">{currentWordType}: </label>
+                <input type="text" id="word" name="word"></input>
+                <Button onClick={handleAddWordClick} buttonText='Add Word'></Button>
+            </div>
+
+        )
+    } else {
+        return (
+            <Button onClick={handleGenerateStoryClick} buttonText="Generate Story!"></Button>
+        )
+    }
+    
 }
