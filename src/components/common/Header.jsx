5510c01e6a87f350feff3436289a6eea2e021d87
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-04-13 09:24:22 -0700

    Changed Header.jsx to have switch statement

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,13 +1,14 @@
 export default function Header({ subheadings, pages, activePage }) {
   const subheading = () => {
-    if (activePage === pages.START) {
-      return subheadings.START;
-    } else if (activePage === pages.WORD_ENTRY) {
-      return subheadings.WORD_ENTRY;
-    } else if (activePage === pages.STORY) {
-      return subheadings.STORY;
-    } else {
-      throw new error("Invalid page");
+    switch (activePage) {
+      case pages.START:
+        return subheadings.START;
+      case pages.WORD_ENTRY:
+        return subheadings.WORD_ENTRY;
+      case pages.STORY:
+        return subheadings.STORY;
+      default:
+        throw new error("Invalid page");
     }
   };
 
