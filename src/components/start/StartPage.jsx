7a5d6ae63d2fe34cddf0fc49f562f21311e0a448
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-04-18 14:57:17 -0700

    Added cta button and modified header styles

--- src/components/start/StartPage.jsx
+++ src/components/start/StartPage.jsx
@@ -1,7 +1,7 @@
 export default function StartPage({ handleClick }) {
   return (
-    <div>
-      <button type="button" onClick={handleClick}>
+    <div className="cta">
+      <button className="cta-button" type="button" onClick={handleClick}>
         Start Adding Words!
       </button>
     </div>
