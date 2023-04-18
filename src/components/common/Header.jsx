b952a43c03866d5222167eb26aedea1915a504ff
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-04-18 14:11:31 -0700

    Added styles to body and header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,9 +2,9 @@ import Subheader from "./Subheader";
 
 export default function Header({ pages, activePage }) {
   return (
-    <>
+    <header>
       <h2>Dad Libs</h2>
       <Subheader pages={pages} activePage={activePage}></Subheader>
-    </>
+    </header>
   );
 }
