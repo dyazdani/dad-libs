2ce7231cdc9974475e24c6b4c43601bac97a0b2f
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-04-20 15:45:24 -0700

    Added first two tests for start page of app

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -7,3 +7,20 @@ import Header from "../components/common/Header";
 import StartPage from "../components/start/StartPage";
 import StoryPage from "../components/story/StoryPage";
 import WordEntryPage from "../components/word_entry/WordEntryPage";
+
+describe("Start page", () => {
+  test("Correct text displayed in header", () => {
+    render(<App />);
+    const header = screen.getByText("Dad Libs");
+    expect(header).toBeDefined();
+  }),
+    test("Correct text displayed in subheader", () => {
+      render(<App />);
+
+      const subheader = screen.getByText(
+        "Ad lib a story about dads, dadding, and dad-tastic shenanigans"
+      );
+
+      expect(subheader).toBeDefined();
+    });
+});
