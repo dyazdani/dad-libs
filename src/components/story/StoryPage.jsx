8799a402f20f69ee45659c8ac18d5c4d0a6b513b
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-04-25 13:09:49 -0700

    Added instruction to story page

--- src/components/story/StoryPage.jsx
+++ src/components/story/StoryPage.jsx
@@ -2,21 +2,29 @@ export default function StoryPage({ story }) {
   const { contents, title } = story;
 
   return (
-    <div className="story-card">
-      <h4>
-        {story.title.map((element, index) => (
-          <span key={"title-" + index}>{element.value}</span>
-        ))}
-      </h4>
-      <p>
-        {story.contents.map((element, index) =>
-          element.type === "string" ? (
-            <span key={"story-part-" + index}>{element.value}</span>
-          ) : (
-            <strong key={"chosen-word-" + index}>{element.value}</strong>
-          )
-        )}
-      </p>
-    </div>
+    <>
+      <header>
+        <h3 id="story-header">
+          Enjoy your rockin' dad story! The words you added are highlighted in
+          green.
+        </h3>
+      </header>
+      <div className="story-card">
+        <h4>
+          {story.title.map((element, index) => (
+            <span key={"title-" + index}>{element.value}</span>
+          ))}
+        </h4>
+        <p>
+          {story.contents.map((element, index) =>
+            element.type === "string" ? (
+              <span key={"story-part-" + index}>{element.value}</span>
+            ) : (
+              <strong key={"chosen-word-" + index}>{element.value}</strong>
+            )
+          )}
+        </p>
+      </div>
+    </>
   );
 }
