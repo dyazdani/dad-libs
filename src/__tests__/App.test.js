bb296bbb37046cbd6cc63341659338f9517831cc
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-05-01 14:47:40 -0700

    Removed import statements from App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,10 +3,6 @@ import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import App from "../App";
-import Header from "../components/common/Header";
-import StartPage from "../components/start/StartPage";
-import StoryPage from "../components/story/StoryPage";
-import WordEntryPage from "../components/word_entry/WordEntryPage";
 
 const testStoryOne = {
   title: [
