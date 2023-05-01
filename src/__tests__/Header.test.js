8cc10c73671b186667d4e0653a1c8363dffe079f
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-05-01 10:18:06 -0700

    Created Header.test.js and added tests to it

---
+++ src/__tests__/Header.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import Header from "../components/common/Header";
+
+describe("Header component renders", () => {
+  test("Header element renders", () => {
+    render(<Header />);
+
+    const header = screen.getByRole("banner");
+
+    expect(header).toBeDefined();
+  });
+  test("h1 element renders", () => {
+    render(<Header />);
+
+    const headerTitle = screen.getByText("Dad Libs");
+
+    expect(headerTitle).toBeDefined();
+  });
+  test("paragraph element renders", () => {
+    render(<Header />);
+
+    const paragraph = screen.getByText(
+      "Ad lib a story about dads, dadding, and dad-tastic shenanigans"
+    );
+
+    expect(paragraph).toBeDefined();
+  });
+});
